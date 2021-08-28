7164f01136c1c8caf8ab191c567213d872def775
Katherine Yoguez <katherinecarlotayoguez@gmail.com>
2021-08-28 13:25:45 -0400

    updated package.json

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const db = require('./config/connection');
 const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
 const { ApolloServerPluginLandingPageGraphQLPlayground } = require("apollo-server-core");
-const { typeDefs, resolvers } = require('./schemas');
+const { typeDefs, resolvers } = require('./schema');
 const { authMiddleware } = require('./utils/auth');
 
 const app = express();
